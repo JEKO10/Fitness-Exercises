852d2460b43529d9f38fc7b5a9e632209a7c5849
Aleksa Bubanja <aleksa3b@gmail.com>
2022-06-21 23:06:18 +0200

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "../assets/Logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState(true);
+
+  return (
+    <nav>
+      <img src={Logo} alt="Logo" />
+      <Link
+        to="/"
+        className={active && "active"}
+        onClick={() => {
+          setActive(true);
+        }}
+      >
+        Home
+      </Link>
+      <Link
+        to="/exercises"
+        className={!active && "active"}
+        onClick={() => {
+          setActive(false);
+        }}
+      >
+        Exercises
+      </Link>
+    </nav>
+  );
 }
 
 export default Navbar;
