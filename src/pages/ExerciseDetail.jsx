5d08e39075a342de4ce300e5dd7042a804aae20a
Aleksa Bubanja <aleksa3b@gmail.com>
2022-07-04 22:54:56 +0200

    ExerciseDetail page

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchData, exerciseOptions } from "../fetchData";
+import Detail from "../components/Detail";
+import ExerciseVideo from "../components/ExerciseVideo";
+import SimilarExercises from "../components/SimilarExercises";
 
 function ExerciseDetail() {
-  return <div>OVO je id</div>;
+  const [exerciseDetail, setExerciseDetail] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const exerciseDb = "https://exercisedb.p.rapidapi.com";
+
+      const exerciseDbData = await fetchData(
+        `${exerciseDb}/exercises/${id}`,
+        exerciseOptions
+      );
+
+      setExerciseDetail(exerciseDbData);
+    };
+
+    fetchDetails();
+  }, [id]);
+
+  return (
+    <section>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideo />
+      <SimilarExercises />
+    </section>
+  );
 }
 
 export default ExerciseDetail;
