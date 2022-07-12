dc115b8c04b2b9d7261f4de2a3fb257f3d03143b
Aleksa Bubanja <aleksa3b@gmail.com>
2022-07-12 21:54:48 +0200

    responsive setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Logo from "../assets/icons/Logo.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer>
-      <div>
+      <Link
+        to="/"
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
+      >
         <img src={Logo} alt="Logo" />
         <p>Golden Gym</p>
-      </div>
+      </Link>
       <h2>Sweat, Smile and Repeat</h2>
     </footer>
   );
