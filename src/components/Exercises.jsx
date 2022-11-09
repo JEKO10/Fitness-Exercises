12bf5cb4d2498d8574ecc1ec9d6992a61205512a
Aleksa Bubanja <aleksa3b@gmail.com>
2022-11-09 19:57:13 +0100

    README

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useEffect } from "react";
 import ExerciseCard from "./ExerciseCard";
 import ReactPaginate from "react-paginate";
 import { fetchData, exerciseOptions } from "../fetchData";
